6fdc1f994d491bf4089b46891923d65814b05e2f
Rene Reede <rene.reede@gmail.com>
2023-03-21 17:19:50 +0200

    Added props to product

--- Mastering-ReactJS/src/routes/Product.jsx
+++ Mastering-ReactJS/src/routes/Product.jsx
@@ -1,11 +1,19 @@
-function Product() {
+import { useState } from "react";
+
+function Product(props) {
+  /*
+  const [SKU, setSKU] = useState("SKU");
+  const [itemName, setitemName] = useState("Item Name");
+  const [price, setprice] = useState(0);
+  const [weight, setweight] = useState(0);
+*/
   return (
     <div className="product-item">
       <input type="checkbox" className=".delete-checkbox" name="" id="" />
-      <p>SKU</p>
-      <p>Name</p>
-      <p>200$</p>
-      <p>Weight:200g</p>
+      <p>{props.sku}</p>
+      <p>{props.itemName}</p>
+      <p>{props.price}</p>
+      <p>{props.weight}</p>
     </div>
   );
 }
--- Mastering-ReactJS/src/routes/Products.jsx
+++ Mastering-ReactJS/src/routes/Products.jsx
@@ -1,14 +1,44 @@
 import Product from "./Product";
 
-function Products() {
+function Products(props) {
   return (
     <div className="products-container">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        sku={2}
+        itemName={"itemName"}
+        price={"price"}
+        weight={"weight"}
+      />
+      <Product
+        sku={2}
+        itemName={"itemName"}
+        price={"price"}
+        weight={"weight"}
+      />
+      <Product
+        sku={3}
+        itemName={"itemName"}
+        price={"price"}
+        weight={"weight"}
+      />
+      <Product
+        sku={4}
+        itemName={"itemName"}
+        price={"price"}
+        weight={"weight"}
+      />
+      <Product
+        sku={5}
+        itemName={"itemName"}
+        price={"price"}
+        weight={"weight"}
+      />
+      <Product
+        sku={6}
+        itemName={"itemName"}
+        price={"price"}
+        weight={"weight"}
+      />
     </div>
   );
 }
